fc5103b23be73d10f97f09d1207fc8ea62a035df
Mario <mpontice@gmail.com>
2014-04-29 18:14:28 -0700

    only allow changing certain fields of posts

--- collections/posts.js
+++ collections/posts.js
@@ -4,6 +4,11 @@ Posts.allow({
 	update: ownsDocument,
 	remove: ownsDocument
 });
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		return (_.without(fieldNames, 'url', 'title').length > 0);
+	}
+});
 
 Meteor.methods({
 	post: function(postAttributes) {
